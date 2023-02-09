08514582372dea7e3a6a1819fedc605d09bb28ac
Simon Achkar <simon@simonachkar.com>
2023-02-09 10:08:19 -0400

    05-camera - Added basic camera app

---
+++ 05-camera/finished/utils/db.js
@@ -0,0 +1,21 @@
+import * as SQLite from "expo-sqlite";
+
+function openDatabase() {
+    if (Platform.OS === "web") {
+      return {
+        transaction: () => {
+          return {
+            executeSql: () => {},
+          };
+        },
+      };
+    }
+  
+    const db = SQLite.openDatabase("images.db");
+  
+    return db;
+  }
+
+  const db = openDatabase();
+
+  export default db;

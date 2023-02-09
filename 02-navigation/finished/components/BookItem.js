4e7e768d8060953045d2f8d10bfb90a00e063a6c
Simon Achkar <simon@simonachkar.com>
2023-02-09 03:05:22 -0400

    02-navigation - Added navigation app

---
+++ 02-navigation/finished/components/BookItem.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, Text, Image, } from 'react-native';
+
+const BookItem = ({ book }) => {
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: book.image }} />
+            <View style={styles.textContainer}>
+                    <Text style={styles.name}>{book.name}</Text>
+    
+                <Text style={styles.author}>{book.author}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        flex: 1,
+        width: '100%'
+    },
+    image: {
+        width: 90,
+        height: 140
+    },
+    textContainer: {
+        marginLeft: 10,
+        width: '90%'
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        width: '90%'
+    },
+    author: {
+        fontSize: 16,  
+        marginTop: 4,
+    }
+});
+
+export default BookItem;
---
+++ 02-navigation/finished/screens/BooksListScreen.js
@@ -0,0 +1,57 @@
+import { useLayoutEffect } from 'react';
+import {View, FlatList, StyleSheet } from 'react-native';
+import BookItem from '../components/BookItem';
+
+import { CATEGORIES, BOOKS } from '../data';
+
+function BooksListScreen({ route, navigation }) {
+    const catId = route.params.categoryId;
+
+    const displayedBooks = BOOKS.filter((bookItem) => {
+        return bookItem.category.indexOf(catId) >= 0;
+      });
+
+      useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+          (category) => category.id === catId
+        ).title;
+    
+        navigation.setOptions({
+          title: categoryTitle,
+        });
+      }, [catId, navigation]);
+    
+      function renderBookItem(itemData) {
+        const item = itemData.item;
+    
+        const bookItemProps = {
+          id: item.id,
+          name: item.name,
+          author: item.author,
+          image: item.image,
+          category: item.category,
+        };
+
+        return <BookItem book={bookItemProps} />
+      }
+
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={displayedBooks}
+            keyExtractor={(item) => item.id}
+            renderItem={renderBookItem}
+          />
+        </View>
+      );
+}
+
+export default BooksListScreen;
+
+
+const styles = StyleSheet.create({
+    container: {
+       flex: 1,
+       width: '95%'
+    },
+})
